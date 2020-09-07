cec510088d19e49b9905a5fc2346e0dc555d01fa
adriangohjw <adriangohjw@gmail.com>
2020-09-07 21:58:15 +0800

    Add User model

--- scansmart-api/db/seeds.rb
+++ scansmart-api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'factory_bot_rails'
+
+10.times do |count|
+  FactoryBot.create(:user, email: "user_#{count}@example.com")
+end
