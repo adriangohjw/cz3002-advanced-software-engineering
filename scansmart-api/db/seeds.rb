c326f4588cfe3eb078b6e2fdc94849998d24adc1
adriangohjw <adriangohjw@gmail.com>
2020-09-08 04:19:47 +0800

    Add Order to seeds

--- scansmart-api/db/seeds.rb
+++ scansmart-api/db/seeds.rb
@@ -8,18 +8,22 @@
 
 require 'factory_bot_rails'
 
+3.times do
+  FactoryBot.create(:store)
+end
+
 10.times do |count|
-  FactoryBot.create(:user, type: "Shopper",
-                           email: "shopper_#{count+1}@example.com",
-                           password: "password")
+  shopper = FactoryBot.create(:user, type: "Shopper",
+                                     email: "shopper_#{count+1}@example.com",
+                                     password: "password")
+  rand(1..5).times do 
+    FactoryBot.create(:order, user: shopper,
+                              store: Store.find(rand(1..Store.count)))
+  end
 end
 
 3.times do |count|
   FactoryBot.create(:user, type: "Staff",
                            email: "staff_#{count+1}@example.com",
                            password: "password")
-end
-
-3.times do
-  FactoryBot.create(:store)
 end
