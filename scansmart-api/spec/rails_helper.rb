4ff8487c7bc86b9f68bdaecaf300770d41357302
adriangohjw <adriangohjw@gmail.com>
2020-09-07 23:35:43 +0800

    Fix bug in CI config file

--- scansmart-api/spec/rails_helper.rb
+++ scansmart-api/spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'support/factory_bot'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,10 +30,6 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
-end
-
-FactoryBot::SyntaxRunner.class_eval do
-  include ActionDispatch::TestProcess
 end
 
 RSpec.configure do |config|
