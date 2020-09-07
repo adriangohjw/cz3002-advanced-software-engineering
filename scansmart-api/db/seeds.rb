221f1ceba2347f6185649de2094e7b70ba54f93d
adriangohjw <adriangohjw@gmail.com>
2020-09-08 03:50:40 +0800

    Fix user unencrypted password for seeds

--- scansmart-api/db/seeds.rb
+++ scansmart-api/db/seeds.rb
@@ -10,12 +10,14 @@ require 'factory_bot_rails'
 
 10.times do |count|
   FactoryBot.create(:user, type: "Shopper",
-                           email: "shopper_#{count+1}@example.com")
+                           email: "shopper_#{count+1}@example.com",
+                           password: "password")
 end
 
 3.times do |count|
   FactoryBot.create(:user, type: "Staff",
-                           email: "staff_#{count+1}@example.com")
+                           email: "staff_#{count+1}@example.com",
+                           password: "password")
 end
 
 3.times do
