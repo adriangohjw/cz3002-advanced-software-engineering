d5bee522c66049247de9ba76798c57edb063e917
adriangohjw <adriangohjw@gmail.com>
2020-09-22 02:05:55 +0800

    Add stripe_customer_identifier field to User

--- scansmart-api/db/schema.rb
+++ scansmart-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_200829) do
+ActiveRecord::Schema.define(version: 2020_09_21_170501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_200829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "type"
+    t.string "stripe_customer_identifier"
   end
 
   add_foreign_key "cart_products", "products"
