19fa1e885d743cc6dd3737ec830d4234da43e6ea
adriangohjw <adriangohjw@gmail.com>
2020-09-24 20:15:46 +0800

    Add compute_total_discounted_price method

--- scansmart-api/app/models/discount.rb
+++ scansmart-api/app/models/discount.rb
@@ -1,3 +1,22 @@
 class Discount < ApplicationRecord
   belongs_to :product
+
+  def self.compute_total_discounted_price(product:, quantity:)
+    return product.price * quantity if product.discounts.blank?
+
+    most_recent_discount = product.latest_discount
+    case most_recent_discount.type
+    when "DiscountSingle"
+      return most_recent_discount.price * quantity
+    when "DiscountBulk"
+      if quantity >= most_recent_discount.bulk_quantity
+        return \
+          (quantity.to_i % most_recent_discount.bulk_quantity.to_i) * product.price \
+          + (quantity.to_i / most_recent_discount.bulk_quantity.to_i) * most_recent_discount.bulk_price
+      else
+        return product.price * quantity
+      end
+    end
+  end
+
 end
