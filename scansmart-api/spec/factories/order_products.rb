0eda8ab67dc856e035fece0dfc0316ece45253ef
adriangohjw <adriangohjw@gmail.com>
2020-09-24 13:58:00 +0800

    Update factories for OrderProduct

--- scansmart-api/spec/factories/order_products.rb
+++ scansmart-api/spec/factories/order_products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_product do
     order { nil }
     product { nil }
-    total_undiscounted_product_price { nil }
+    total_undiscounted_product_price { 0 }
     quantity { 1 }
     total_discount_amount { 0 }
   end
