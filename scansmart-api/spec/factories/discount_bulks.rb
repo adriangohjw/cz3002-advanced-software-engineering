164d6add2ba1cf1135c2799abea7bb9da1387c9f
adriangohjw <adriangohjw@gmail.com>
2020-09-24 20:15:20 +0800

    Update factories for DiscountBulk

--- scansmart-api/spec/factories/discount_bulks.rb
+++ scansmart-api/spec/factories/discount_bulks.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     product { nil }
     from_date { Time.zone.now + rand(0..10).days }
     to_date { from_date + rand(0..7).days }
+    bulk_quantity { rand(1..3) }
   end
 end
