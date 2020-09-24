6988f194cb4a9f449387b3336d3ec0d68e78058e
adriangohjw <adriangohjw@gmail.com>
2020-09-24 13:58:12 +0800

    Add Orders API

--- scansmart-api/config/routes.rb
+++ scansmart-api/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post 'users/' => 'users#create'
   put 'users/' => 'users#update'
 
+  get 'orders/' => 'orders#show'
+  get 'users/:user_id/orders' => 'orders#show_all_for_shopper'
+
   post 'movements/' => 'movements#create'
 
 end
