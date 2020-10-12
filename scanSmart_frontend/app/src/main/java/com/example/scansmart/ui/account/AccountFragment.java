de67fe3515313e7ae89f10b59dd9610d648cb04a
CharlotteChng <chng.charlotte@gmail.com>
2020-10-12 13:57:01 +0800

    profile ui done

--- scanSmart_frontend/app/src/main/java/com/example/scansmart/ui/account/AccountFragment.java
+++ scanSmart_frontend/app/src/main/java/com/example/scansmart/ui/account/AccountFragment.java
@@ -17,20 +17,10 @@ import com.example.scansmart.R;
 
 public class AccountFragment extends Fragment {
 
-    private AccountViewModel accountViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        accountViewModel =
-                ViewModelProviders.of(this).get(AccountViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_account);
-        accountViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
