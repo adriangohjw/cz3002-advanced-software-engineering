d2ca0be70bf94512b247a413833699f1053b0714
adriangohjw <adriangohjw@gmail.com>
2020-11-08 17:16:24 +0800

    Remove unused users API route

--- scansmart-api/config/routes.rb
+++ scansmart-api/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   get 'users/authenticate' => 'users#authenticate'
-  get 'users/' => 'users#show'
   get 'users/:id' => 'users#show'
   post 'users/' => 'users#create'
   put 'users/' => 'users#update'
